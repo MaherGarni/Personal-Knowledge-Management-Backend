97b004b8cb268f7b72e4281620bc1a1b278340c2
Maher <qarnimaher@gmail.com>
2025-11-02 10:21:41 +0300

    update functionality for categories

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Home, CategoriesIndex
+from .views import Home, CategoriesIndex, CategoryDetail
 
 urlpatterns = [
     path('', Home.as_view(), name='home'), #comment
     path('categories/', CategoriesIndex.as_view(), name='categories-index'),
+    path('categories/<int:category_id>/', CategoryDetail.as_view(), name='category-detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import generics, status
 from .models import Category
 from .serializers import CategorySerializer
-from django.db.models import F
+from django.shortcuts import get_object_or_404
 # Define the home view
 class Home(APIView):
     def get(self, request):
@@ -16,13 +16,29 @@ class CategoriesIndex(APIView):
     serializer_class = CategorySerializer
     categories = Category.objects.all()
 
-    
-
     def get(self, request):
         try:
             queryset = Category.objects.all()
             serializer = self.serializer_class(queryset, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as err:
-            # print(serializer.errors)
+            return Response({'error': str(err)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+class CategoryDetail(APIView):
+    serializer_class = CategorySerializer   
+    lookup_field = 'id'
+    
+    def put(self, request, category_id):
+        print(request.data, "line32")
+        try:
+            category = get_object_or_404(Category, id=category_id)
+            serializer = self.serializer_class(category, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                updated_categories = Category.objects.all()
+                serializer = self.serializer_class(updated_categories, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            print(serializer.errors, "line39")
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as err:
             return Response({'error': str(err)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
